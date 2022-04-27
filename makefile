4b5dcbc946e14afd7235e680cca45bf07d692519
tybitsfox <tyyyyt@163.com>
2022-04-27 09:18:35 +0800

    2022-4-27 update function

--- makefile
+++ makefile
@@ -1,5 +1,9 @@
-tf03:tf03.c ./iniget/iniget.h ./iniget/iniget.c
-	gcc -o tf03 tf03.c -I./iniget/ -I/home/tian/cprogram/include
+ALL: tf03 iniget
+	@echo "begin makes"
+iniget: ./iniget/iniget.c ./iniget/iniget.h
 	make -C '$(shell pwd)/iniget/'
+tf03:tf03.c ./iniget/iniget.h
+	gcc -o tf03 tf03.c -I./iniget/ -I/home/tian/cprogram/include
 clean:
-	rm tf03
+	-rm tf03
+	make -C '$(shell pwd)/iniget/' clean
--- tf03.c
+++ tf03.c
@@ -1,11 +1,27 @@
 #include"iniget.h"
 /*这是一个conky的cpu温度的获取程序.
  	tybitsfox		2022-4-26
+add:
+	2022-4-27 添加对amd显卡的温度获取
+	使用设备索引号区分不同的设备，
+	index=0		cpu温度
+	index=1		amdgpu温度
+	对于使用nvidia闭源驱动的设备，索引码为2
+	index=2		nvidiagpu 温度
+	index=3		开源驱动显卡温度
  */
+#define	amd		"amdgpu"
+
 int main(int argc,char **argv)
 {
-	char ch[256],tmp[32];
-	int f,i;
+	char ch[512],tmp[32];
+	int f,i,j;
+	if(argc != 2)
+	{
+		printf("Usage:\n%s tempindex\nindex=0: coretemp\nindex=1: amd gputemp\n\
+index=2: nvidia gputemp\nindex=3: opensourc gputemp\n",argv[0]);
+		return 0;
+	}
 	f=open(SFILE,O_RDONLY);
 	if(f<=0)
 		return 0;
@@ -15,6 +31,20 @@ int main(int argc,char **argv)
 		goto err_01;
 	close(f);
 	zero(tmp);
+	j=atoi(argv[1]);
+	switch(j)
+	{
+	case 0: //coretemp
+		break;
+	case 1: //amd gputemp
+		break;
+	case 2://nvidia gputemp
+		break;
+	case 3://open source gputemp
+		break;
+	default:
+		return 0;
+	}
 	f=open(ch,O_RDONLY);
 	if(f<=0)
 		return 0;
